b8d5f367b669aba3dc157bcb87287b56835bf671
CaioGamer11 <86723009+CaioGamer11@users.noreply.github.com>
2021-06-30 09:20:26 -0300

    Add files via upload

---
+++ Arroz Japonês.py
@@ -0,0 +1,15 @@
+
+print("\t\t\tArroz Japonês")
+print("\t\tIngredientes:")
+print("1- 1 1/2 Xícara(Chá)de arroz japonês(grão do tipo longo para culinária oriental)")
+print("2- 3 Xícaras (chá) de água")
+print("3- Gergelim branco a gosto para servir")
+print("\t\tModo de Preparo:")
+print("""1- Em uma peneira, lave o arroz sob água corrente até a água sair menos branca – é essencial lavar para deixar os 
+grãos mais grudadinhos. Transfira para uma panela pequena e cubra com a água.""")
+print("""2- Leve ao fogo médio e deixe cozinhar por 10 minutos, até começar a ferver. Abaixe o fogo e deixe cozinhar por 
+mais 10 minutos com a tampa entreaberta, ou até o arroz absorver toda a água – ele fica levemente úmido e 
+bem grudadinho.""")
+print("""3- Desligue o fogo, tampe a panela e deixe o arroz terminar de cozinhar no próprio vapor por mais 5 minutos.
+Na hora de servir, salpique com gergelim branco. Ele é perfeito para acompanhar preparos orientais.""")
+
