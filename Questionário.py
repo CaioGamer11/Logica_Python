0717815b5def038c74bb69d1af946c84db727578
CaioGamer11 <86723009+CaioGamer11@users.noreply.github.com>
2021-07-07 09:58:50 -0300

    Add files via upload

---
+++ Questionário.py
@@ -0,0 +1,8 @@
+Idade_User= int(input("Digite sua idade:"))
+Sal_User= float(input("Digite seu salário:"))
+Adicional_User= Sal_User*0.2
+
+print("Sua idade é:", Idade_User)
+print("Seu salário é: R$", Sal_User)
+print("Adicional: R$", Adicional_User)
+print("Total: R$", Sal_User+Adicional_User)
