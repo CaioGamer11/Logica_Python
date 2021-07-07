0f7c47a88c6c078c7971482fc38db6c7ce5831fe
CaioGamer11 <86723009+CaioGamer11@users.noreply.github.com>
2021-07-07 09:50:45 -0300

    Add files via upload

---
+++ Login.py
@@ -0,0 +1,3 @@
+Nome= input("Qual é seu nome?\nMeu nome é ")
+Idade= int(input("Quantos anos você tem?\nMinha idade é "))
+Senha= int(input("Qual sua senha?(Só pode ser em números)\nMinha senha é "))
