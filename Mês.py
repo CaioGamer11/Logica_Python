53fbf23f7e3dd1870683f9a63cb4a0f5745be6df
CaioGamer11 <86723009+CaioGamer11@users.noreply.github.com>
2021-07-14 09:35:46 -0300

    Add files via upload

---
+++ Mês.py
@@ -0,0 +1,27 @@
+Mes= int(input("Esolha um número de 1 á 12:"))
+if Mes == 1:
+    print("Estamos no mês de janeiro.")
+elif Mes == 2:
+    print("Estamos no mês de fevereiro.")
+elif Mes == 3:
+    print("Estamos no mês de março.")
+elif Mes == 4:
+    print("Estamos no mês de abril.")
+elif Mes == 5:
+    print("Estamos no mês de maio.")
+elif Mes == 6:
+    print("Estamos no mês de junho.")
+elif Mes == 7:
+    print("Estamos no mês de julho.")
+elif Mes == 8:
+    print("Estamos no mês de agosto.")
+elif Mes == 9:
+    print("Estamos no mês de setembro.")
+elif Mes == 10:
+    print("Estamos no mês de outubro.")
+elif Mes == 11:
+    print("Estamos no mês de novembro.")
+elif Mes == 12:
+    print("Estamos no mês de dezembro.")
+else:
+    print("Este não é um mês válido.")
