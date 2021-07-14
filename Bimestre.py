ff435a00ea2ebbf733237ebcda638a0cfcf45517
CaioGamer11 <86723009+CaioGamer11@users.noreply.github.com>
2021-07-14 09:40:40 -0300

    Add files via upload

---
+++ Bimestre.py
@@ -0,0 +1,11 @@
+Bim= int(input("Qual seu bimestre?"))
+if Bim == 1:
+    print("Você está no primeiro bimestre.")
+elif Bim == 2:
+    print("Você está no segundo bimestre.")
+elif Bim == 3:
+    print("Você está no terçeiro bimestre.")
+elif Bim == 4:
+    print("Você está no quarto bimestre.")
+else:
+    print("Isto não é um bimestre válido.")
