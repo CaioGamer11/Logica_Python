b821f029e28ebc95355cac05ccb86fa957794107
CaioGamer11 <86723009+CaioGamer11@users.noreply.github.com>
2021-08-11 08:39:24 -0300

    Add files via upload

---
+++ Login(SEGURO).py
@@ -0,0 +1,6 @@
+User= input("Digite seu usuário...")
+Password= int(input("Digite sua senha...(Somente números!)"))
+if (User=="Caio") and (Password==1234567890):
+    print("Você está logado com sucesso.")
+else:
+    print("Login ou senha incorretos.")
